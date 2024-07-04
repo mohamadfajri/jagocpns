ed3b72093f9af916de2f112f9bddddbdd8d3b962
Khaqul Huda <khaqulhudaa@gmail.com>
2024-07-05 01:37:19 +0700

    fix: sidebar

--- client/src/components/app/Sidebar.jsx
+++ client/src/components/app/Sidebar.jsx
@@ -1,11 +1,15 @@
 import { Link, Outlet } from 'react-router-dom';
 import logo from '../../assets/images/logo-extend.png';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 const Sidebar = () => {
   const location = useLocation();
+
+  const [open, setOpen] = useState();
   return (
     <>
       <button
+        onClick={() => setOpen(true)}
         data-drawer-target='default-sidebar'
         data-drawer-toggle='default-sidebar'
         aria-controls='default-sidebar'
@@ -27,9 +31,8 @@ const Sidebar = () => {
           />
         </svg>
       </button>
-
       <aside
-        id='default-sidebar'
+        id='efault-sidebar'
         className='fixed bg-white top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 border-r border-gray-300 dark:border-gray-700 dark:bg-black'
         aria-label='Sidebar'
       >
@@ -259,6 +262,243 @@ const Sidebar = () => {
           </div>
         </div>
       </aside>
+      {open && (
+        <div>
+          <div
+            onClick={() => {
+              setOpen(false);
+            }}
+            className='fixed left-0 right-0 top-0 bottom-0 z-30 bg-black opacity-35'
+          ></div>
+          <aside className='fixed sm:hidden bg-white top-0 left-0 z-40 w-64 h-screen border-r border-gray-300 dark:border-gray-700 dark:bg-black'>
+            <div className='h-full flex flex-col'>
+              <div className='p-4'>
+                <img src={logo} alt='logo' className='rounded-lg' />
+              </div>
+              <div className='h-fit px-3 py-24 overflow-y-auto bg-white dark:bg-black'>
+                <ul className='space-y-4 font-medium'>
+                  <li>
+                    <Link
+                      to={'/app/dashboard'}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${
+                        location.pathname === '/app/dashboard'
+                          ? 'bg-gray-100'
+                          : 'hover:bg-gray-200 dark:hover:bg-gray-700'
+                      }`}
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path d='M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z' />
+                        <path d='m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z' />
+                      </svg>
+
+                      <span className='ms-3'>Dashboard</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={'/app/mytryouts'}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${
+                        location.pathname === '/app/mytryouts'
+                          ? 'bg-gray-100'
+                          : 'hover:bg-gray-200 dark:hover:bg-gray-700'
+                      }`}
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path d='M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z' />
+                        <path d='M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z' />
+                        <path d='M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z' />
+                      </svg>
+
+                      <span className='ms-3'>Tryout Saya</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={'/app/tryoutstore'}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${
+                        location.pathname === '/app/tryoutstore'
+                          ? 'bg-gray-100'
+                          : 'hover:bg-gray-200 dark:hover:bg-gray-700'
+                      }`}
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                      <span className='ms-3'>Beli Tryout</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={'/app/ranks'}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${
+                        location.pathname === '/app/ranks'
+                          ? 'bg-gray-100'
+                          : 'hover:bg-gray-200 dark:hover:bg-gray-700'
+                      }`}
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z'
+                          clipRule='evenodd'
+                        />
+                        <path
+                          fillRule='evenodd'
+                          d='M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                      <span className='ms-3'>Ranking</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={'/app/topup'}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${
+                        location.pathname === '/app/topup'
+                          ? 'bg-gray-100'
+                          : 'hover:bg-gray-200 dark:hover:bg-gray-700'
+                      }`}
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path d='M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z' />
+                        <path
+                          fillRule='evenodd'
+                          d='M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+
+                      <span className='ms-3'>TopUp Saldo</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={'/app/information'}
+                      className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${
+                        location.pathname === '/app/information'
+                          ? 'bg-gray-100'
+                          : 'hover:bg-gray-200 dark:hover:bg-gray-700'
+                      }`}
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                      <span className='ms-3'>Informasi</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={'/'}
+                      className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                      <span className='ms-3'>F.A.Q</span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className='p-4'>
+                <ul>
+                  <li>
+                    <Link
+                      to={'/app/profile'}
+                      className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                        className='size-6'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+
+                      <span className='ms-3'>Profile</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={'/auth/signout'}
+                      className='flex items-center p-2 text-red-600 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
+                    >
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth={1.5}
+                        stroke='currentColor'
+                        className='size-6'
+                      >
+                        <path
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          d='M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75'
+                        />
+                      </svg>
+
+                      <span className='ms-3'>Sign Out</span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </aside>
+        </div>
+      )}
       <Outlet />
     </>
   );
