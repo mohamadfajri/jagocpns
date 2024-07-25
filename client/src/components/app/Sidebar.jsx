61929bafe66fb2e26f9fdf4e05f754748b2183d5
Khaqul Huda <khaqulhudaa@gmail.com>
2024-07-25 14:08:18 +0700

    fix:image

--- client/src/components/app/Sidebar.jsx
+++ client/src/components/app/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from 'react-router-dom';
-import logo from '../../assets/images/logo-extend.png';
+import logo from '../../assets/images/logo-extend-black.png';
 import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
 const Sidebar = () => {
