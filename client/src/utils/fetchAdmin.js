8d5b4fe7abd6ec380f2b2de6912c0ba0b70d236d
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-09 01:36:54 +0700

    feat: create fetching func for development

--- client/src/utils/fetchAdmin.js
+++ client/src/utils/fetchAdmin.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { useAdmin } from '../stores/useAdmin';
+import axios from "axios";
+import { useAdmin } from "../stores/useAdmin";
 
 const fetchAdmin = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/admin`,
@@ -18,4 +18,21 @@ fetchAdmin.interceptors.request.use(
   }
 );
 
-export { fetchAdmin };
+const fetchAdminDev = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL_DEV}/api/admin`,
+});
+
+fetchAdminDev.interceptors.request.use(
+  (config) => {
+    const { token } = useAdmin.getState();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { fetchAdmin, fetchAdminDev };
