40fa38de19186cd7097dbe63040abe0313c7f384
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-12 18:19:01 +0700

    style: change active tab color

--- client/src/components/app/Sidebar.jsx
+++ client/src/components/app/Sidebar.jsx
@@ -117,7 +117,7 @@ const Sidebar = () => {
                 <Link
                   to={"/app/ranks"}
                   className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${
-                    location.pathname === "/app/ranks"
+                    location.pathname.includes("/app/ranks") 
                       ? "bg-[#FFCB01]"
                       : "hover:bg-gray-200 dark:hover:bg-gray-700"
                   }`}
