69fa5471c0edea6d830fd10a496dc9be1f8ea0db
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-12 18:20:25 +0700

    feat: create route for get user rank

--- server/src/routes/public.js
+++ server/src/routes/public.js
@@ -1,33 +1,37 @@
-const express = require('express');
+const express = require("express");
 const {
   getInformationByType,
   getLinks,
-} = require('../controllers/public/information');
-const { getUserRankingsByTryout } = require('../controllers/public/ranking');
-const { createOwnerr } = require('../controllers/public/user');
+} = require("../controllers/public/information");
+const {
+  getUserRankingsByTryout,
+  getUserRankById,
+} = require("../controllers/public/ranking");
+const { createOwnerr } = require("../controllers/public/user");
 const {
   getAllTryout,
   getTryoutById,
   getFreeTryouts,
   getIsOnlineStatus,
-} = require('../controllers/public/tryoutList');
+} = require("../controllers/public/tryoutList");
 const {
   forgotPasswordHandler,
   resetPasswordHandler,
-} = require('../controllers/public/forgotpassword');
-const {getUserCount} = require("../controllers/admin/analytic");
+} = require("../controllers/public/forgotpassword");
+const { getUserCount } = require("../controllers/admin/analytic");
 
 const router = express.Router();
 
-router.get('/public/information/:type', getInformationByType);
-router.get('/public/rank', getUserRankingsByTryout);
-router.get('/public/tryouts', getAllTryout);
-router.get('/public/tryout/:id', getTryoutById);
-router.get('/public/freetryouts', getFreeTryouts);
-router.get('/public/links', getLinks);
-router.get('/public/isOnlineTryout/:id', getIsOnlineStatus);
-router.post('/public/forgot-password', forgotPasswordHandler);
-router.post('/public/reset-password', resetPasswordHandler);
-router.get('/public/usercount', getUserCount);
+router.get("/public/information/:type", getInformationByType);
+router.get("/public/rank", getUserRankingsByTryout);
+router.get("/public/rank/tryout/:tryoutListId/user/:id", getUserRankById);
+router.get("/public/tryouts", getAllTryout);
+router.get("/public/tryout/:id", getTryoutById);
+router.get("/public/freetryouts", getFreeTryouts);
+router.get("/public/links", getLinks);
+router.get("/public/isOnlineTryout/:id", getIsOnlineStatus);
+router.post("/public/forgot-password", forgotPasswordHandler);
+router.post("/public/reset-password", resetPasswordHandler);
+router.get("/public/usercount", getUserCount);
 
 module.exports = router;
