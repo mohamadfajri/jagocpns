4698afec24e0fbf66f31fe467c1cf9a7acc27229
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-12 18:18:35 +0700

    feat: new page for rendeer rank table

---
+++ client/src/components/app/Ranking/RankPage.jsx
@@ -0,0 +1,76 @@
+import TableRank from "./TableRank.jsx";
+import PaginationRank from "./PaginationRank.jsx";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetcher } from "../../../utils/fetcher.js";
+import RankingCard from "./RankingCard.jsx";
+export default function RankPage() {
+  const location = useLocation();
+  const pathParts = location.pathname.split("/");
+  const id = pathParts[pathParts.length - 1];
+  const [tryOutName, setTryOutName] = useState();
+  const [userData, setUserData] = useState([]);
+  const [userRank, setUserRank] = useState();
+
+  useEffect(() => {
+    const fetchUserDataAndRank = async () => {
+      try {
+        const userResponse = await fetcher("/user");
+        setUserData(userResponse.data);
+
+        if (userResponse.data.userId) {
+          try {
+            const rankResponse = await fetcher(
+              `/public/rank/tryout/${id}/user/${userResponse.data.userId}`
+            );
+            setUserRank(rankResponse.data.rank);
+          } catch (error) {
+            if (error.response?.status === 404) {
+              setUserRank("Anda Belum Mengikuti Tryout Ini");
+            } else {
+              console.error("Error fetching user rank", error);
+            }
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUserDataAndRank();
+  }, [id]);
+
+  useEffect(() => {
+    const getTryOutName = async () => {
+      try {
+        const response = await fetcher(`/public/tryout/${id}`);
+        setTryOutName(response.data.title);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTryOutName();
+  }, [id]);
+
+  return (
+    <div>
+      <div className="sm:p-10 sm:ml-64 sm:mt-0 pb-16 dark:bg-black min-h-screen">
+        <p className="text-2xl font-bold">Daftar Ranking</p>
+        <p className="font-bold text-3xl">{tryOutName}</p>
+        <div className="grid grid-cols-5 gap-5">
+          <div className="mt-5 col-span-4">
+            <TableRank />
+            <PaginationRank />
+          </div>
+          <div className="flex flex-col items-center">
+            <RankingCard
+              title={tryOutName}
+              name={userData.name}
+              email={userData.email}
+              ranking={userRank}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
