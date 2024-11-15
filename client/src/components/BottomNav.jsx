010b02260c4c14fbffe9129bbe193555358bf7a0
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-15 17:35:14 +0700

    feat: new bottm nav

--- client/src/components/BottomNav.jsx
+++ client/src/components/BottomNav.jsx
@@ -46,7 +46,7 @@ const BottomNav = () => {
                 <path d='M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z' />
               </svg>
             </Link>
-            <h1 className='text-xs'>Tryout</h1>
+            <h1 className='text-xs'>Tryout Saya</h1>
           </li>
           <li className='flex flex-col items-center'>
             <Link to={'/app/mytryouts'}>
@@ -61,7 +61,7 @@ const BottomNav = () => {
                 <path d='M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z' />
               </svg>
             </Link>
-            <h1 className='text-xs'>Bimbel</h1>
+            <h1 className='text-xs'>Bimbel Saya</h1>
           </li>
           <li className='flex flex-col items-center'>
             <Link to={'/app/tryoutstore'}>
@@ -80,6 +80,7 @@ const BottomNav = () => {
             </Link>
             <h1 className='text-xs'>Beli Tryout</h1>
           </li>
+
           <li className='flex flex-col items-center'>
             <Link to={'/app/tryoutstore'}>
               <svg
@@ -95,66 +96,10 @@ const BottomNav = () => {
                 />
               </svg>
             </Link>
-            <h1 className='text-xs'>Beli Bibmel</h1>
+            <h1 className='text-xs'>Beli Bimbel</h1>
           </li>
-          <li className='flex flex-col items-center'>
-            <Link to={'/app/ranks'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='currentColor'
-                className='size-6'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z'
-                  clipRule='evenodd'
-                />
-                <path
-                  fillRule='evenodd'
-                  d='M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </Link>
-            <h1 className='text-xs'>Rank</h1>
-          </li>
-          <li className='flex flex-col items-center'>
-            <Link to={'/app/topup'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='currentColor'
-                className='size-6'
-              >
-                <path d='M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z' />
-                <path
-                  fillRule='evenodd'
-                  d='M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </Link>
-            <h1 className='text-xs'>Topup</h1>
-          </li>
-          <li className='flex flex-col items-center'>
-            <Link to={'/app/information'}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='currentColor'
-                className='size-6'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </Link>
-            <h1 className='text-xs'>Info</h1>
-          </li>
-          <li className='flex flex-col items-center'>
+         
+          {/* <li className='flex flex-col items-center'>
             <Dropdown
               arrowIcon={false}
               inline
@@ -186,7 +131,7 @@ const BottomNav = () => {
               </Dropdown.Item>
             </Dropdown>
             <h1 className='text-xs'>Profile</h1>
-          </li>
+          </li> */}
         </ul>
       </div>
     </>
