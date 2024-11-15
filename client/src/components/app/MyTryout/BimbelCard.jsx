8801cf9e01d325f22a4581c2f3bc5b692c5815ef
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-16 02:32:18 +0700

    style: responsive

--- client/src/components/app/MyTryout/BimbelCard.jsx
+++ client/src/components/app/MyTryout/BimbelCard.jsx
@@ -1,20 +1,35 @@
-export default function BimbelCard() {
+import { Link } from "react-router-dom";
+
+export default function BimbelCard(props) {
+  const { title, image, buttonText, price, url } = props;
   return (
     <div>
-      <p>Bibmel Card</p>
-      <div className="border rounded-xl">
-        <div className="grid grid-cols-3 ">
-          <div className="col-span-2">
-            <p>Iamge</p>
+      <div className="border rounded-xl max-h-52 xl:w-5/6">
+        <div className="grid grid-cols-4">
+          <div className="col-span-3 h-52">
+            <img
+              src={image}
+              alt=""
+              className="object-fill w-full h-full rounded-s-xl"
+            />
           </div>
-          <div className="flex flex-col justify-between p-4">
+          <div className="flex flex-col justify-around px-2 xl:pb-5">
             <div>
-              <p className="font-bold">Bimbel SKB</p>
+              <p className="font-bold xl:text-xl">{title}</p>
             </div>
             <div className="mt-14">
-              <p>1-5 November 2024</p>
-              <p className="font-bold">SKB</p>
-              <button className="bg-[#FFCB01] rounded-lg py-2 px-10">Link Group Whatsapp</button>
+              <p className="text-xs xl:text-base">1-5 November 2024</p>
+              <p className="text-xs xl:text-base font-bold">SKB</p>
+              {price && (
+                <p className="text-xs md:text-lg xl:text-xl font-bold">
+                  {price}
+                </p>
+              )}
+              <Link to={url}>
+                <button className="bg-[#FFCB01] mt-1 text-xs rounded-lg xl:py-2 xl:px-10 font-bold w-full">
+                  {buttonText}
+                </button>
+              </Link>
             </div>
           </div>
         </div>
