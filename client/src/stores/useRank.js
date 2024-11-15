59fdb6787e1aaa909f863de77a7619f0a8a54162
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-15 17:39:59 +0700

    feat: add limit to global storage

--- client/src/stores/useRank.js
+++ client/src/stores/useRank.js
@@ -1,12 +1,14 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useRank = create((set) => ({
   active: null,
   page: 1,
+  limit: 10,
   totalPages: 1,
   setTotalPage: (value) => set({ totalPages: value }),
   setPage: (value) => set({ page: value }),
   setActive: (value) => set({ active: value }),
+  setLimit: (limit) => set({ limit }),
 }));
 
 export { useRank };
