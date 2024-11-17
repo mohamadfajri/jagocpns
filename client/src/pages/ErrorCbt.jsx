f57ae8ddcad5f13d9cf368b9823746b99fae63b6
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-17 12:58:12 +0700

    feat: redirect button to mytryout

--- client/src/pages/ErrorCbt.jsx
+++ client/src/pages/ErrorCbt.jsx
@@ -1,31 +1,31 @@
-import { Button } from 'flowbite-react';
-import { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Button } from "flowbite-react";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 const ErrorCbt = () => {
   useEffect(() => {
-    localStorage.removeItem('cbt-storage');
+    localStorage.removeItem("cbt-storage");
   }, []);
   const { error } = useParams();
   const message = decodeURIComponent(error);
   return (
     <>
-      <section className='bg-white dark:bg-gray-900'>
-        <div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
-          <div className='mx-auto max-w-screen-sm text-center'>
-            <h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500'>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm text-center">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
               500
             </h1>
-            <p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>
+            <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
               Internal Error
             </p>
-            <p className='mb-4 text-lg font-light text-gray-500 dark:text-gray-400'>
+            <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
               {message}
             </p>
             <Link
-              to={'/app/dashboard'}
-              className='inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4'
+              to={"/app/mytryouts"}
+              className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
             >
-              <Button color={'success'}>Back to Homepage</Button>
+              <Button color={"success"}>Back to Homepage</Button>
             </Link>
           </div>
         </div>
