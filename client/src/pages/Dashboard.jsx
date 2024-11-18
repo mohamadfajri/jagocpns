2ab92b1d46d7236d7af330844ed3292bcabb3afc
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-18 21:41:35 +0700

    feat: responsive

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -27,11 +27,11 @@ const Dashboard = () => {
       <div>
         <div className="mb-20 px-5 py-0 xl:px-5 xl:py-5 md:ml-[270px] xl:ml-64 xl:mb-0 dark:bg-black min-h-screen">
           <div className="xl:gap-5 xl:pe-32">
-            <div className="h-36 sm:h-64 xl:h-80 mb-3 mt-3 xl:mb-5 2xl:h-96">
+            <div className="h-36 sm:h-64 xl:h-[25rem] mb-3 mt-3 xl:mb-5">
               <Carousel>
                 <img src={dashboardBanner} alt="..." className="" />
                 <img src={dashboardBanner2} alt="..." className="" />
-                <img src={dashboardBanner3} alt="..." className="h-full" />
+                <img src={dashboardBanner3} alt="..." className="" />
               </Carousel>
             </div>
 
