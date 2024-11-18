c5f8a4c70f6316c92a298e7d56d0cc8535a0c32f
michaelrynm <133634767+michaelrynm@users.noreply.github.com>
2024-11-18 20:00:13 +0700

    feat: add responsive

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,7 +2,7 @@ import ProfileTab from '../components/app/Profile/ProfileTab';
 
 const ProfilePage = () => {
   return (
-    <div className='sm:p-10 sm:ml-64 dark:bg-black min-h-screen'>
+    <div className='p-5 sm:ml-64 dark:bg-black min-h-screen'>
       <div>
         <p className='font-medium text-2xl'>Profie</p>
       </div>
